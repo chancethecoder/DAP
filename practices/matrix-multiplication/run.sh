bcce403d7da411e8e545615419275edac3180536
Youngkyun <youngkyun@Youngkyunui-MacBook-Pro.local>
2017-09-17 18:55:26 +0900

    Update

--- practices/matrix-multiplication/run.sh
+++ practices/matrix-multiplication/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 TARGET_CPU_EXAMPLE="cpu_practice_exe"
 TIMEOUT_FLAGS="--signal=SIGKILL"
-TIMEOUT_TIME="3s"
+TIMEOUT_TIME="3"
 GPROF_OUTPUT="gmon.out"
 GPROF_LOG="log.txt"
+
+function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 for N in {500..2000..250}
 do
     echo "start program with N: $N"
-    SIG=`timeout $TIMEOUT_FLAGS $TIMEOUT_TIME ./$TARGET_CPU_EXAMPLE $N || echo "0"`
+    SIG=`timeout $TIMEOUT_TIME ./$TARGET_CPU_EXAMPLE $N || echo "0"`
     echo "$SIG"
     if [ "$SIG" -ne "0" ] ; then
         echo "비정상 종료"
