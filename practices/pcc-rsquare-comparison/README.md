4809d89d7a661ba4ad395f19a593a3970395e7af
chancethecoder <kyg516@interpark.com>
2017-12-19 11:59:30 +0000

    Add pcc vs r-square

---
+++ practices/pcc-rsquare-comparison/README.md
@@ -0,0 +1,17 @@
+# PCC vs R square
+
+Comparison between two values - PCC(Pearson Correlation Coefficient) and R squre
+
+## Requirements
+
+* virtualenv
+
+## How to run
+
+```bash
+git clone https://github.com/chancethecoder/DAP [directory_name]
+cd [directory_name]/practices/pcc-rsquare-comparison
+source env/bin/activate
+pip install -r requirements.txt
+python test.py
+```
---
+++ practices/pcc-rsquare-comparison/test.py
@@ -0,0 +1,26 @@
+# test on linux, used python3(3.4.3)
+#
+
+from scipy import stats
+import numpy as np
+import json
+
+def calc_and_print(x, y):
+  slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+  pcc, p_value = stats.pearsonr(x, y)
+  print("R square:", r_value**2)
+  print("Pearson's r:", pcc)
+  print("diff:", (pcc - r_value**2))
+  print("\n")
+
+if __name__ == "__main__":
+  with open('data.json') as data_file:
+	  json_file = json.load(data_file)
+
+  data = json_file['data']
+  x = [d['x'] for d in data]
+  y = [d['y'] for d in data]
+
+  for i in range(10):
+    upto = int(len(x)/(10 - i))
+    calc_and_print(x[:upto], y[:upto])
